8f2b3ecbc0abb2deb56a876d04edb823a0417445
Prabal Pathak <prabal01pathak@gmail.com>
2021-11-22 21:27:19 +0530

    add static root

--- jeetests/settings.py
+++ jeetests/settings.py
@@ -266,11 +266,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-"""
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
-"""
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
