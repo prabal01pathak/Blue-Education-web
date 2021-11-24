bd3c3774d409a19578ea4dddecd8f2d56021d736
Prabal Pathak <prabal01pathak@gmail.com>
2021-11-25 00:46:29 +0530

    debug True

--- jeetests/settings.py
+++ jeetests/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['blueeducation.herokuapp.com','0.0.0.0','localhost','127.0.0.1']
