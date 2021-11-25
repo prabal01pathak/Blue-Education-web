34d3f32c01a8b7d3adcf19657456939317291016
Prabal Pathak <prabal01pathak@gmail.com>
2021-11-26 00:28:02 +0530

    alert on tab change

--- static/tests/js/show_assign.js
+++ static/tests/js/show_assign.js
@@ -2,6 +2,8 @@ var selectAll = document.querySelector(".select-all input[type=checkbox]");
 var selectAllCheckbox = document.querySelectorAll("form .questions input[type=checkbox]");
 var assignSelect = document.querySelector("#do-action"); 
 var assignThings = document.querySelector("form .assign-things"); 
+var assignThingsHide = document.querySelector(".assign_title_button");
+
 
 selectAll.addEventListener("click", function() {
     for (var i = 0; i < selectAllCheckbox.length; i++) {
@@ -13,15 +15,16 @@ selectAll.addEventListener("click", function() {
     }
 });
 
-assignSelect.addEventListener("change", function() {
+//Assign items to other papers
+assignSelect.addEventListener("click", function() {
     var assign = assignSelect.value;
-    console.log(assign);
     if (assign == "assign_to_other") {
-        console.log("assign to other");
         assignThings.style.transform = "translateX(0%)"; 
     }else {
-        console.log("assign to self");
         assignThings.style.transform = "translateX(100%)";
     };
 });
 
+assignThingsHide.addEventListener("click",() => {
+	assignThings.style.transform="translateX(100%)";
+});
--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static 
-from .views import index,home,show_title,get_subject,add_questions,edit_question,show_assign_or_delete,edit_title,add_title,terms
+from .views import index,home,show_title,get_subject,add_questions,edit_question,show_assign_or_delete,edit_title,add_title,terms,filter_pages,contact_us
 
 app_name = 'jeetests'
 urlpatterns = [
@@ -15,6 +15,8 @@ urlpatterns = [
         path("add/questions/<str:model>/<int:title_id>/", add_questions, name='add-question'),
         path("edit/question/<int:question_id>/<str:model>/<int:title_id>/", edit_question, name='edit-question'),
         path("terms" , terms,name="terms"),
+        path("filter/<str:filter_type>/" , filter_pages,name="filter_type"),
+        path("contact/" , contact_us,name="contact"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
